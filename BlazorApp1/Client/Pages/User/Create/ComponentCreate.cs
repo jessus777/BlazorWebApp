d77a4866b925a1b31f150fef849bf928e5d5e151
jesus.jimenez <jesus.jimenez@mother.travel>
2022-04-05 00:04:12 -0500

    Agregar archivos de proyecto.

---
+++ BlazorApp1/Client/Pages/User/Create/ComponentCreate.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Client.Pages.User.Create
+{
+    public partial class ComponentCreate
+    {
+        private string Name;
+
+        private string? validInput;
+        private void Send()
+        {
+            //if (string.IsNullOrEmpty(name))
+            //{
+            //    Console.WriteLine(name);
+            //}
+            
+            //validInput = string.IsNullOrEmpty(Name) ? "is-invalid" : "is-valid";
+        }
+
+        private void ValidateInput(ChangeEventArgs e)
+        {
+            //validInput = string.IsNullOrEmpty(Name) ? "is-invalid" : "is-valid";
+            Name = e.Value.ToString();
+            //validInput = string.IsNullOrEmpty(Name) ? "is-invalid" : "is-valid";
+        }
+    }
+}
